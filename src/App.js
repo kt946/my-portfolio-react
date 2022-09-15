ebf1d025ac2e2b8f3830ab24c67b3b9d4658733a
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-15 16:37:12 -0400

    added About component and hero image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-
+        <About></About>
       </main>
     </div>
   );
---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function About() {
+  return(
+    <section id="about" className='text-white'>
+      <div className='hero p-5'>
+        <h2>
+          Hello World!
+        </h2>
+      </div>
+      <div className='mx-5 my-3'>
+        <h2 className='section-title'>
+          About Me
+        </h2>
+        <div className='fs-5'>
+          <p >
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio exercitationem quo suscipit recusandae natus totam doloremque, hic vel, animi fugit nemo itaque sunt voluptatibus consequatur. Porro possimus repellendus debitis vel.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam adipisci ipsam facere ex rerum sed dolores repellendus rem culpa nulla veniam quam numquam sunt tempora quia, praesentium quaerat dolorem consequatur.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum consectetur dignissimos explicabo natus, blanditiis, omnis beatae illum quis aperiam ad quas veritatis amet aut alias culpa, dicta aliquid est! Amet!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
