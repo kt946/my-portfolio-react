f6a4ea6c4f59fbfad22f6b27e5bad59291b1db84
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-15 15:12:34 -0400

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <main>
+
+      </main>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+function Header() {
+  return(
+    <header>
+      <h1 id='name' className='text-bg-dark p-3'>Kyle Tang</h1>
+    </header>
+  );
+}
+
+export default Header;
