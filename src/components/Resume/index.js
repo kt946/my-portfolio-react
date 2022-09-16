b76408d3ed681c903eb3e8d130007e90c5b357e3
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-16 19:21:26 -0400

    updated Resume component with resume link and proficiencies

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,72 @@
 import React from 'react';
 
 function Resume() {
+  const frontEndSkills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'jQuery',
+    'Responsive Design',
+    'React',
+    'Bootstrap'
+  ];
+
+  const backEndSkills = [
+    'APIs',
+    'Node',
+    'Express',
+    'MySQL, Sequelize',
+    'MongoDb, Mongoose',
+    'REST'
+  ];
+
   return(
-    <section id="resume" className='mx-5 py-3 text-white'>
-      <h2 className='section-title'>
+    <section id="resume" className="mx-5 py-3 text-white">
+      <h2 className="section-title">
         Resume
       </h2>
+      <div className="mt-5 fs-5">
+        Download my <span>
+          <a 
+            href="/" 
+            className="text-decoration-none"
+          >
+            resume
+          </a>
+        </span>
+      </div>
+      <div className="my-3">
+        <div className="row g-4">
+          <div className="col">
+            <div className="border rounded">
+              <h3 className="fs-5 py-3 px-2 text-center border-bottom">
+                Front-end Proficiencies
+              </h3>
+              <ul className="fs-5">
+                {frontEndSkills.map(skill => (
+                  <li key={skill}>
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="col">
+            <div className="border rounded">
+              <h3 className="fs-5 py-3 px-2 text-center border-bottom">
+                Back-end Proficiencies
+              </h3>
+              <ul className="fs-5">
+                {backEndSkills.map(skill => (
+                  <li key={skill}>
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
