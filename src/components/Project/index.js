18545256175a5230812c61b5a52a958eb0cb6f2c
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-22 15:54:55 -0400

    small edit to buttons on project cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -23,9 +23,11 @@ function Project(props) {
             <div className="card-body">
               <p className="card-text">{project.description}</p>
             </div>
-            <div className="card-footer d-flex justify-content-between">
-              <a href={project.deployed} className="btn btn-primary">Deployed App</a>
-              <a href={project.repo} className="btn btn-success">GitHub Repository</a>
+            <div className="card-footer d-flex justify-content-between align-items-center">
+              <a href={project.deployed} className="btn btn-primary">Visit Website</a>
+              <a href={project.repo} className="fs-2">
+                <i className="bi bi-github"></i>
+              </a>
             </div>
           </div>
         </div>
